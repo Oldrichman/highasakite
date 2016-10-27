083d542e4615ff8e865da9c795a60cd8043059e9
Wesslin Jenni <a1500957@myy.haaga-helia.fi>
2016-10-27 08:32:58 +0300

    database tables modified

--- database/insert_into_table.sql
+++ database/insert_into_table.sql
@@ -32,6 +32,22 @@ INSERT INTO Paikkakunta (nimi)
 /* Tapahtuman lis‰‰minen */
 	
 INSERT INTO Tapahtuma (TapLuojaId, teema, paikka, TapNimi, pvm, aika, paikkakunta, lisatiedot)
-	VALUES (2, 2, 'Ravintola', 'Viininmaisteluilta', '25-10-2016', '18:00', 1, 'Dresscode: smart casual.');	
-
+	VALUES (2, 2, 'Ravintola', 'Viininmaisteluilta', '2016-10-25', '18:00', 1, 'Dresscode: smart casual.');	
+			
+INSERT INTO Tapahtuma (TapLuojaId, teema, paikka, TapNimi, pvm, aika, paikkakunta, lisatiedot)	
+	VALUES (3, 3, 'Kotona', 'Peli-ilta', '2016-11-11', '15:00', 1, 'Pelaillaan lautapelej‰, jee!');
+			
+INSERT INTO Tapahtuma (TapLuojaId, teema, paikka, TapNimi, pvm, aika, paikkakunta, lisatiedot)
+	VALUES (4, 3, 'Kotona', 'Lanit', '2016-11-23', '15:00', 2, 'Lanitellaan yhdess‰');
 	
+/* Osallistujien lis‰‰minen */
+	
+INSERT INTO Osallistuja (etunimi, sukunimi, email)
+	VALUES ('Jaakko', 'Jaakkima', 'jaakkojaakkima@gmail.com'),
+	       ('Jenna', 'J‰‰r‰', 'jennajaara@luukku.com'),
+	       ('Juha', 'Juupas', 'juhajuupas@gmail.com');
+	      
+INSERT INTO TapahtumaOsallistuja (osallistuja_id, tapahtuma_id)
+	VALUES (2, 2),
+	       (1, 3);
+			
